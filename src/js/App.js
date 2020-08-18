a4955d97c0c84349aa06fc4a15225b53383705fe
David Chavarría <dabbidchavarria@gmail.com>
2020-08-18 12:21:47 -0600

    Generate rtn-card inside index.html from App.js

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,27 @@
 import '../scss/main.scss';
 import './components/Controls';
 import './components/Cards';
+import { FourSquareStore } from './stores/FourSquareStore';
+
+class App {
+
+    constructor() {
+        this.controls = document.querySelector('rtn-controls');
+        this.cardSection = document.getElementById('card-section');
+        window.addEventListener('ready-data', this.displayCard.bind(this));
+    }
+
+    displayCard(event) {
+        console.log(event.detail);
+
+        const {items: recommendations} = event.detail;
+
+        for (const obj of recommendations) {
+            const card = document.createElement('rtn-card');
+            card.recommendations = obj.venue;
+            this.cardSection.append(card);
+        }
+    }
+}
+
+new App();
