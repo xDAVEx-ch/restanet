ebedf14ff3f3be4d24e27362b27a7a890fc61db5
David Chavarría <dabbidchavarria@gmail.com>
2020-08-30 12:56:29 -0600

    Add the new component The logic for the new component usage was set up

--- src/js/components/Cards.js
+++ src/js/components/Cards.js
@@ -1,9 +1,11 @@
+import { ModalService } from '../services/ModalService';
+
 export class Cards extends HTMLElement {
 
-    constructor(){
+    constructor() {
         super();
 
-        this.attachShadow({ mode : 'open' });
+        this.attachShadow({ mode: 'open' });
         this.venue = null;
         this.venuePhotos = null;
         this.venueTips = null;
@@ -19,6 +21,7 @@ export class Cards extends HTMLElement {
                     background-color: var(--color-light-shades);
                     color: var(--color-text);
                     box-shadow: 0px 0px 12px -3px rgba(52,63,62,1);
+                    min-height: 190px;
                 }
 
                 blockquote{
@@ -46,38 +49,47 @@ export class Cards extends HTMLElement {
             </style>
 
             <div class="card">
-                <img src="${this.venuePhotos[0].prefix+ '300x200' +this.venuePhotos[0].suffix}" alt="Example image from Foursquare Venue">
+                <img src="${this.venuePhotos}" alt="Example image from Foursquare Venue">
                 <div class="group">
                     <h3>${this.venue.name}</h3>
-                    <blockquote>${this.venueTips[0].text}</blockquote>
+                    <blockquote>${this.venueTips}</blockquote>
                     <p class="location"><b>Ubicación:</b> ${this.analyseInfo()}</p>
                 </div>
             </div>
         `;
     }
 
-    set recommendations(value){
+    set recommendations(value) {
         this.venue = value;
         this.render();
     }
 
-    set details(value){
-        let {items: venuePhotos} = value.photosData;
-        let {items: venueTips} = value.tipsData;
+    set details(value) {
 
-        this.venuePhotos = venuePhotos;
-        this.venueTips = venueTips;
+        if (value.photosData) {
+            let { items: venuePhotos } = value.photosData;
+            let { items: venueTips } = value.tipsData;
+
+            this.venuePhotos = venuePhotos[0].prefix+ '300x200' +venuePhotos[0].suffix;
+            this.venueTips = venueTips[0].text;
+        }else{
+            this.venuePhotos = 'https://picsum.photos/id/1060/300/200';
+            this.venueTips = 'Me llevaron para celebrar mi cumpleaños. Un buen lugar para comer en familia. Que rico todo el menú.';
+
+            ModalService.add('rtn-modal-notification');
+        }
+
     }
 
-    analyseInfo(){
+    analyseInfo() {
 
         let location = '';
 
-        if(this.venue.location.city === undefined && this.venue.location.address === undefined) {
+        if (this.venue.location.city === undefined && this.venue.location.address === undefined) {
             location = 'No disponible';
-        }else if (this.venue.location.city === undefined){
+        } else if (this.venue.location.city === undefined) {
             location = this.venue.location.address;
-        }else{
+        } else {
             location = `${this.venue.location.city}, ${this.venue.location.address}`;
         }
 
